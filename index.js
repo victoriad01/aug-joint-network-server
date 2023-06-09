da6b650866f5ff96a45652b78d0c4e5746877f8d
victoriad01 <94958284+victoriad01@users.noreply.github.com>
2023-06-09 10:29:56 +0100

    clean out

--- index.js
+++ index.js
@@ -33,15 +33,11 @@ app.use((req, res, next) => {
   next()
 })
 
-// Set up HTTPS options
 const httpsOptions = {
   key: fs.readFileSync('./cert/key.pem'),
   cert: fs.readFileSync('./cert/cert.pem'),
-
-  // cert: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
 }
 
-// Connect to mongodb using mongoose and called in app.listen below
 const connect = () => {
   mongoose
     .connect(process.env.MONGO_URL)
